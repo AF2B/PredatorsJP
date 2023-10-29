e4a20c231e4ecdc7b1d5f8a66b46fe933af895af
andre.borba <dev_andre.filipe@outlook.com>
2023-10-29 20:51:50 -0300

    feat: model

--- PredatorsJP/PredatorModel.swift
+++ PredatorsJP/PredatorModel.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+import SwiftUI
+
+struct PredatorModel: Codable, Identifiable {
+    let id: Int
+    let name: String
+    let type: String
+    let movies: [String]
+    let movieScenes: [MovieScene]
+    let link: String
+    
+    func typeOverlay() -> Color {
+        switch type {
+        case "land": return .brown
+        case "air": return .teal
+        case "sea": return .blue
+        default: return .brown
+        }
+    }
+}
+
+struct MovieScene: Codable, Identifiable {
+    let id: Int
+    let movie: String
+    let sceneDescription: String
+}

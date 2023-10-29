66c38cd22fbef2e8184431ad602ba77a769620d6
andre.borba <dev_andre.filipe@outlook.com>
2023-10-29 17:38:49 -0300

    Initial Commit

---
+++ PredatorsJP/PredatorsJPApp.swift
@@ -0,0 +1,17 @@
+//
+//  PredatorsJPApp.swift
+//  PredatorsJP
+//
+//  Created by André Filipe Fonsêca Borba on 29/10/23.
+//
+
+import SwiftUI
+
+@main
+struct PredatorsJPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
